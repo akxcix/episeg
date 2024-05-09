805a3d6871d1d35e83b29af07e9d84ad3aaec235
akxcix <git@akxcix.com>
2024-05-09 15:03:48 -0400

    add dataloader and demo notebook

---
+++ src/data.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+import shutil
+import logging
+import h5py
+import numpy as np
+from PIL import Image
+
+def prepare_datasets(data_link, image_dir, mask_dir, train_image_dir, train_mask_dir, test_dir):
+    if not os.path.exists(image_dir):
+        logging.info("Downloading and extracting dataset...")
+        os.makedirs(image_dir, exist_ok=True)
+        subprocess.run([
+            "curl", "-L", data_link, "-o", os.path.join(image_dir, "epi.tgz")
+        ], check=True)
+        subprocess.run(["tar", "-xzvf", os.path.join(image_dir, "epi.tgz"), "-C", image_dir], check=True)
+        os.remove(os.path.join(image_dir, "epi.tgz"))
+        logging.info("Dataset downloaded and extracted.")
+
+    os.makedirs(train_image_dir, exist_ok=True)
+    os.makedirs(train_mask_dir, exist_ok=True)
+    os.makedirs(data_link, exist_ok=True)
+
+    logging.info("Sorting images and masks...")
+    for image in os.listdir(image_dir):
+        if image.endswith(".tif"):
+            base = os.path.splitext(image)[0]
+            mask_path = os.path.join(mask_dir, f"{base}_mask.png")
+
+            if os.path.isfile(mask_path):
+                shutil.move(os.path.join(image_dir, image), train_image_dir)
+                shutil.move(mask_path, train_mask_dir)
+            else:
+                shutil.move(os.path.join(image_dir, image), test_dir)
+
+    logging.info("Images and masks sorted.")
+
+def create_h5_files(hdf5_file_path, train_image_dir, train_mask_dir):
+    with h5py.File(hdf5_file_path, 'w') as h5f:
+        image_dataset = h5f.create_group('images')
+        mask_dataset = h5f.create_group('masks')
+
+        for image_name in os.listdir(train_image_dir):
+            if image_name.endswith(".tif"):
+                image_path = os.path.join(train_image_dir, image_name)
+                base = os.path.splitext(image_name)[0]
+                mask_path = os.path.join(train_mask_dir, f"{base}_mask.png")
+                
+                image = np.array(Image.open(image_path))
+                mask = np.array(Image.open(mask_path))
+                
+                image_dataset.create_dataset(base, data=image, compression="gzip")
+                mask_dataset.create_dataset(base, data=mask, compression="gzip")
+
+        logging.info("HDF5 file created with images and masks.")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
-from data_preparation import prepare_datasets
+import data
+import dataloader
 import logging
+import os
+
+DATA_LINK = "https://andrewjanowczyk.com/wp-static/epi.tgz"
+
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
+IMAGE_DIR = os.path.join(BASE_DIR, "data")
+MASK_DIR = os.path.join(IMAGE_DIR, "masks")
+TRAIN_IMAGE_DIR = os.path.join(IMAGE_DIR, "train/images")
+TRAIN_MASK_DIR = os.path.join(IMAGE_DIR, "train/masks")
+TEST_DIR = os.path.join(IMAGE_DIR, "test")
+HDF5_FILE_PATH = os.path.join(IMAGE_DIR, "train_data.h5")
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info("Preparing datasets...")
-    prepare_datasets()
-    logging.info("Datasets prepared.")
+    data.prepare_datasets(DATA_LINK, IMAGE_DIR, MASK_DIR, TRAIN_IMAGE_DIR, TRAIN_MASK_DIR, TEST_DIR)
+    data.create_h5_files(HDF5_FILE_PATH, TRAIN_IMAGE_DIR, TRAIN_MASK_DIR)
+
+    dataset = dataloader.SegmentationDataset(HDF5_FILE_PATH)
+    dataset.show_random_image()
+
+
+    
 
 if __name__ == '__main__':
+    print(os.path.dirname(__file__))
     main()
